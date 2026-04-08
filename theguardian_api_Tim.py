c558f45612fc5918e3e49cb0226339cfaf5dad7c
Tim Morozov <tim7.moroz0v@yandex.ru>
2026-04-08 22:55:13 +0300

    theguardian api base

--- theguardian_api_Tim.py
+++ theguardian_api_Tim.py
@@ -1,1 +1,24 @@
 import requests
+import json
+from guardian_api_key import theguardian_key
+
+
+base_url = 'https://content.guardianapis.com/search'
+
+params = {
+    'q': 'climate change',
+    'from-date': '2026-01-01',
+    'to-date': '2026-04-01',
+    'order-by': 'relevance',
+    'show-blocks': 'body',
+    'api-key': theguardian_key
+}
+
+response = requests.get(base_url, params=params).json()
+
+
+
+with open('pretty_json.json', 'w', encoding='utf-8') as f:
+    json.dump(response, f, indent=4)
+
+print(response)
