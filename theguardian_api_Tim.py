5a29c849eea58c89b3d301f757449bb7b66ec314
Tim Morozov <tim7.moroz0v@yandex.ru>
2026-04-09 14:04:41 +0300

    guardian api updated, pandas df

--- theguardian_api_Tim.py
+++ theguardian_api_Tim.py
@@ -6,17 +6,15 @@ from guardian_api_key import theguardian_key
 base_url = 'https://content.guardianapis.com/search'
 
 params = {
-    'q': 'AAPL',
-    'page': 1,
+    'q': 'APPL',
     'from-date': '2026-01-01',
     'to-date': '2026-04-01',
     'order-by': 'relevance',
-    'show-blocks': 'main,body',
+    'show-blocks': 'body',
     'api-key': theguardian_key
 }
 
 response = requests.get(base_url, params=params).json()
-
 
 
 with open('pretty_json.json', 'w', encoding='utf-8') as f:
